5d319be9db557caef2f4b496682920487721e508
Alex Adler <69403909+dadler89@users.noreply.github.com>
2021-01-07 06:48:31 -0600

    Save point

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ app.get("/", (req, res) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?zip=62002&units=imperial&appid=${process.env.OPEN_WEATHER}`
 https.get(url, (response) => {
   console.log(response.statusCode);
+
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      console.log(temp);
+      console.log(weatherDescription);
+    })
 })
-  
   res.send("Server is up!!")
 })
 
